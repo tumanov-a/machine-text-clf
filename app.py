2ccecc2c8ee0ef35a01dc3f8524c4e2428ccfc8a
tumanov-a <tumanov94@gmail.com>
2022-12-22 14:16:09 +0000

    Add mysql database support, docker.

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask, render_template, request
 from analyzer import Analyzer
 
+from flask_mysqldb import MySQL
+
 app = Flask(__name__, template_folder='templates')
 
 analyzer = Analyzer('checkpoints/bert_model_0_1.pt')
+
+app.config['MYSQL_HOST'] = '0.0.0.0'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '1234567890'
+app.config['MYSQL_DB'] = 'flask'
+app.config['MYSQL_PORT'] = 5060
+ 
+mysql = MySQL(app)
 
 @app.route('/')
 def tryit():
@@ -13,13 +23,53 @@ def tryit():
 def about():
     return render_template('about.html')
 
+@app.route('/db')
+def db():
+    cur = mysql.connection.cursor()
+    # cur.execute('''USE flask''')
+    # cur.execute('''CREATE TABLE nlg_label (
+    #     id INT PRIMARY KEY AUTO_INCREMENT,
+    #     text TEXT,
+    #     label iNT
+    # )''')
+    s = cur.execute('''SELECT * FROM nlg_label''')
+    rv = cur.fetchall()
+    mysql.connection.commit()
+    return render_template('db.html', db=rv)
+
 @app.route('/predict_label', methods=['POST'])
 def predict_label():
     text = request.form['text']
-    print(text)
-    pred_label = analyzer.predict_label(text)
-    print(pred_label)
-    return render_template('tryit.html', context={'label': pred_label})
+    pred_label, pred_prob = analyzer.predict_label(text)
+    return render_template('tryit.html', context={'label': pred_label, 'prob': pred_prob, 'text': text})
+
+@app.route('/insert_label', methods=['POST'])
+def insert_label():
+    text = request.form['text']
+    label = request.form['label']
+    correct = request.form['insert_label_button']
+    if correct == 'correct':
+        label = label
+    elif correct == 'incorrect':
+        labels = ['Machine', 'Human'].pop(label)
+        label = labels[0]
+    cur = mysql.connection.cursor()
+    cur.execute('''INSERT INTO nlg_label (text, label) VALUES(%s, %s)''', (text, label))  
+    mysql.connection.commit()
+    # mysql.connection.close()
+    return render_template('tryit.html')
+
+def create_database():
+    cur = mysql.connection.cursor()
+    cur.execute('''USE flask''')
+    cur.execute('''CREATE TABLE nlg_label (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        text TEXT,
+        label iNT
+    )''')  
+    mysql.connection.commit()
+    # mysql.connection.close()
 
 if __name__ == '__main__':
-    app.run()
+    create_database()
+    app.run(debug=True)
